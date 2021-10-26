4bcefe9c8ffc55960ac4bd58c6313161f353bb5a
max <sibo_jian_29903549@163.com>
2021-10-26 15:26:10 +0800

    add lock when modify VMQueryRegistry registries

--- Sources/Core/VMQueryRegistry.swift
+++ Sources/Core/VMQueryRegistry.swift
@@ -13,17 +13,31 @@ class VMQueryRegistry {
   
   static let shared = VMQueryRegistry()
   
+  private let _lock = NSLock()
+  
   private var registries: [AtsaniKey: Any] = [:]
   
   init() {
     
   }
   
+  // 加锁, 防止竞争 registries 引起的崩溃
   func register<RequestContext, Response>(forIdentifier identifier: AtsaniKey, anyQuery: VMAnyQuery<RequestContext, Response>) {
+    self._lock.lock()
+    defer {
+      self._lock.unlock()
+    }
+    
     self.registries[identifier] = anyQuery
   }
   
+  // 加锁, 防止竞争 registries 引起的崩溃
   func unregister(forIdentifier identifier: AtsaniKey) {
+    self._lock.lock()
+    defer {
+      self._lock.unlock()
+    }
+    
     self.registries.removeValue(forKey: identifier)
   }
   
